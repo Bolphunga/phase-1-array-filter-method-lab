87f281cfe9bd3f995f44de9285dcee152425dfff
Bolphunga <aymendj@yahoo.com>
2023-08-10 02:22:46 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution here
+// // Code your solution here
+
+
+
+function findMatching(array, name) {
+    return array.filter(driver=>(
+        driver.toLowerCase() === name.toLowerCase()
+    )
+             )
+}
+
+
+
+function fuzzyMatch(array, name){
+    return array.filter(driver=>(
+        driver.substring(0, 1) === name.substring(0, 1)
+    )
+            )
+}
+
+
+function matchName(array, name) {
+    return array.filter(key=>key.name === name)
+}
+
